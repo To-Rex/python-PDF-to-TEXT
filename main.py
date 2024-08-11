6c90069d8d82925b723df6a3a7eff1efe17a52fd
to-rex <torex.amaki@gmail.com>
2024-08-11 08:04:59 +0500

    first

--- main.py
+++ main.py
@@ -55,6 +55,62 @@ def fetch_all_pdf():
         abort(500, description=str(e))
 
 
+#get all page get all page request {
+#     "book": [
+#         {
+#             "text": "",
+#             "page": 0
+#         },
+#         {
+#             "text": "",
+#             "page": 0
+#         }
+#     ],
+#     "appPages": 100
+# }
+@app.route('/all')
+def fetch_all_pdf_page():
+    pdf_url = "https://ildizkitoblari.uz/public/files/pdf1.pdf"
+    try:
+        if pdf_url not in pdf_cache:
+            response = requests.get(pdf_url)
+            response.raise_for_status()
+            pdf_cache[pdf_url] = BytesIO(response.content)
+        with pdfplumber.open(pdf_cache[pdf_url]) as pdf:
+            total_pages = len(pdf.pages)
+            book = []
+            for page_number in range(1, total_pages+1):
+                page = pdf.pages[page_number - 1]
+                text = page.extract_text()
+                book.append({"text": text, "page": page_number})
+            return jsonify({"book": book, "appPages": total_pages})
+    except requests.exceptions.RequestException as e:
+        abort(500, description=f"Error fetching PDF: {str(e)}")
+    except Exception as e:
+        abort(500, description=str(e))
+
+
+@app.route('/pdf/<path:url>')
+def fetch_all_pdf_page_url(url):
+    pdf_url = url
+    try:
+        if pdf_url not in pdf_cache:
+            response = requests.get(pdf_url)
+            response.raise_for_status()
+            pdf_cache[pdf_url] = BytesIO(response.content)
+        with pdfplumber.open(pdf_cache[pdf_url]) as pdf:
+            text = ""
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n"
+            return jsonify({"text": text}) if text else jsonify({"text": "No text found in the document."})
+    except requests.exceptions.RequestException as e:
+        abort(500, description=f"Error fetching PDF: {str(e)}")
+    except Exception as e:
+        abort(500, description=str(e))
+
+
 @app.route('/pdf/<path:url>/')
 def fetch_pdf(url):
     pdf_url = url
