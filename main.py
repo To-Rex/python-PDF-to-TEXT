71bfa3087d14ebe9269132f2c58c529539504a7b
to-rex <torex.amaki@gmail.com>
2024-08-11 06:38:13 +0500

    first

--- main.py
+++ main.py
@@ -35,6 +35,44 @@ def pdf_page(page_number):
         abort(500, description=str(e))
 
 
+@app.route('/pdf/')
+def fetchall_pdf():
+    pdf_url = "https://ildizkitoblari.uz/public/files/pdf1.pdf"
+    try:
+        if pdf_url not in pdf_cache:
+            response = requests.get(pdf_url)
+            response.raise_for_status()
+            pdf_cache[pdf_url] = BytesIO(response.content)
+        with pdfplumber.open(pdf_cache[pdf_url]) as pdf:
+            text = ""
+            for page in pdf.pages:
+                text += page.extract_text()
+            return text if text else "No text found in the document."
+    except requests.exceptions.RequestException as e:
+        abort(500, description=f"Error fetching PDF: {str(e)}")
+    except Exception as e:
+        abort(500, description=str(e))
+
+
+@app.route('/pdf/<path:url>/')
+def fetch_pdf(url):
+    pdf_url = url
+    try:
+        if pdf_url not in pdf_cache:
+            response = requests.get(pdf_url)
+            response.raise_for_status()
+            pdf_cache[pdf_url] = BytesIO(response.content)
+        with pdfplumber.open(pdf_cache[pdf_url]) as pdf:
+            text = ""
+            for page in pdf.pages:
+                text += page.extract_text()
+            return text if text else "No text found in the document."
+    except requests.exceptions.RequestException as e:
+        abort(500, description=f"Error fetching PDF: {str(e)}")
+    except Exception as e:
+        abort(500, description=str(e))
+
+
 @app.route('/pdf/', defaults={'page_number': 1})
 @app.route('/pdf/<path:url>/<int:page_number>')
 def fetch_pdf_page(url, page_number):
@@ -56,6 +94,7 @@ def fetch_pdf_page(url, page_number):
         abort(500, description=f"Error fetching PDF: {str(e)}")
     except Exception as e:
         abort(500, description=str(e))
+
 
 
 if __name__ == '__main__':
